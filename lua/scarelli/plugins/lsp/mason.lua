f65805c33a13e81b6c3fa72eb1252856c4e87839
pedro scarelli <pvscarelli@gmail.com>
2025-04-22 15:34:11 -0300

    fix: mason config

--- lua/scarelli/plugins/lsp/mason.lua
+++ lua/scarelli/plugins/lsp/mason.lua
@@ -1,182 +1,160 @@
 return {
-  'williamboman/mason.nvim',
-  lazy = false,
-  dependencies = {
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    'hrsh7th/cmp-nvim-lsp',
-    'neovim/nvim-lspconfig',
-    -- "saghen/blink.cmp",
-  },
-  config = function()
-    -- import mason and mason_lspconfig
-    local mason = require 'mason'
-    local mason_lspconfig = require 'mason-lspconfig'
-    local mason_tool_installer = require 'mason-tool-installer'
-
-    -- NOTE: Moved from lspconfig.lua
-    -- import lspconfig plugin
-    local lspconfig = require 'lspconfig'
-    local cmp_nvim_lsp = require 'cmp_nvim_lsp' -- import cmp-nvim-lsp plugin
-    local capabilities = cmp_nvim_lsp.default_capabilities() -- used to enable autocompletion (assign to every lsp server config)
-
-    -- enable mason and configure icons
-    mason.setup {
-      ui = {
-        icons = {
-          package_installed = '✓',
-          package_pending = '➜',
-          package_uninstalled = '✗',
-        },
-      },
-
-      registries = {
-        'github:Crashdummyy/mason-registry', -- Registro customizado para o Roslyn
-        'github:mason-org/mason-registry', -- Registro padrão (já incluso por padrão)
-      },
-    }
-
-    mason_lspconfig.setup {
-      -- servers for mason to install
-      ensure_installed = {
-        'lua_ls',
-        -- "ts_ls", currently using a ts plugin
-        'html',
-        'cssls',
-        'tailwindcss',
-        'gopls',
-        'emmet_ls',
-        'emmet_language_server',
-        -- "eslint",
-        'marksman',
-      },
-      -- auto install configured servers (with lspconfig)
-      automatic_installation = true,
-    }
-
-    mason_tool_installer.setup {
-      ensure_installed = {
-        'prettier', -- prettier formatter
-        'stylua', -- lua formatter
-        'isort', -- python formatter
-        'pylint',
-        'clangd',
-        'denols',
-        { 'eslint_d', version = '13.1.2' },
-      },
-    }
-
-    -- NOTE: Moved from lspconfig.lua
-
-    mason_lspconfig.setup_handlers {
-      -- default handler for installed servers
-      function(server_name)
-        lspconfig[server_name].setup {
-          capabilities = capabilities,
-        }
-      end,
-      ['emmet_ls'] = function()
-        -- configure emmet language server
-        lspconfig['emmet_ls'].setup {
-          capabilities = capabilities,
-          filetypes = {
-            'html',
-            'typescriptreact',
-            'javascriptreact',
-            'css',
-            'sass',
-            'scss',
-            'less',
-            'svelte',
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = function()
+      require('mason').setup {
+        ui = {
+          icons = {
+            package_installed = '✓',
+            package_pending = '➜',
+            package_uninstalled = '✗',
           },
-        }
-      end,
-      ['lua_ls'] = function()
-        -- configure lua server (with special settings)
-        lspconfig['lua_ls'].setup {
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              -- make the language server recognize "vim" global
-              diagnostics = {
-                globals = { 'vim' },
-              },
-              completion = {
-                callSnippet = 'Replace',
-              },
-              workspace = {
-                -- make language server aware of run time files
-                library = {
-                  [vim.fn.expand '$VIMRUNTIME/lua'] = true,
-                  [vim.fn.stdpath 'config' .. '/lua'] = true,
+        },
+        registries = {
+          'github:Crashdummyy/mason-registry',
+          'github:mason-org/mason-registry',
+        },
+      }
+    end,
+  },
+
+  {
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = { 'williamboman/mason.nvim' },
+    config = function()
+      local mason_lspconfig = require 'mason-lspconfig'
+      mason_lspconfig.setup {
+        ensure_installed = {
+          'lua_ls',
+          'html',
+          'cssls',
+          'tailwindcss',
+          'gopls',
+          'emmet_ls',
+          'emmet_language_server',
+          'marksman',
+        },
+        automatic_installation = true,
+      }
+
+      local lspconfig = require 'lspconfig'
+      local cmp_nvim_lsp = require 'cmp_nvim_lsp'
+      local capabilities = cmp_nvim_lsp.default_capabilities()
+
+      mason_lspconfig.setup_handlers {
+        -- handler padrão para todos
+        function(server_name)
+          lspconfig[server_name].setup { capabilities = capabilities }
+        end,
+        ['emmet_ls'] = function()
+          lspconfig.emmet_ls.setup {
+            capabilities = capabilities,
+            filetypes = {
+              'html',
+              'typescriptreact',
+              'javascriptreact',
+              'css',
+              'sass',
+              'scss',
+              'less',
+              'svelte',
+            },
+          }
+        end,
+        ['lua_ls'] = function()
+          lspconfig.lua_ls.setup {
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                diagnostics = { globals = { 'vim' } },
+                completion = { callSnippet = 'Replace' },
+                workspace = {
+                  library = {
+                    [vim.fn.expand '$VIMRUNTIME/lua'] = true,
+                    [vim.fn.stdpath 'config' .. '/lua'] = true,
+                  },
                 },
               },
             },
-          },
-        }
-      end,
-      ['emmet_language_server'] = function()
-        lspconfig.emmet_language_server.setup {
-          filetypes = {
-            'css',
-            'eruby',
-            'html',
-            'javascript',
-            'javascriptreact',
-            'less',
-            'sass',
-            'scss',
-            'pug',
-            'typescriptreact',
-          },
-          -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
-          -- **Note:** only the options listed in the table are supported.
-          init_options = {
-            ---@type table<string, string>
-            includeLanguages = {},
-            --- @type string[]
-            excludeLanguages = {},
-            --- @type string[]
-            extensionsPath = {},
-            --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-            preferences = {},
-            --- @type boolean Defaults to `true`
-            showAbbreviationSuggestions = true,
-            --- @type "always" | "never" Defaults to `"always"`
-            showExpandedAbbreviation = 'always',
-            --- @type boolean Defaults to `false`
-            showSuggestionsAsSnippets = false,
-            --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-            syntaxProfiles = {},
-            --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-            variables = {},
-          },
-        }
-      end,
-      ['denols'] = function()
-        lspconfig.denols.setup {
-          capabilities = capabilities,
-          root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'), -- Attach only if these files exist
-        }
-      end,
-      ['ts_ls'] = function()
-        lspconfig.ts_ls.setup {
-          capabilities = capabilities,
-          root_dir = function(fname)
-            -- Use tsserver unless a Deno-specific config is present
-            local util = lspconfig.util
-            return not util.root_pattern('deno.json', 'deno.jsonc')(fname)
-              and util.root_pattern('tsconfig.json', 'package.json', 'jsconfig.json', '.git')(fname)
-          end,
-          single_file_support = false,
-          init_options = {
-            preferences = {
-              includeCompletionsWithSnippetText = true,
-              includeCompletionsForImportStatements = true,
+          }
+        end,
+        ['emmet_language_server'] = function()
+          lspconfig.emmet_language_server.setup {
+            filetypes = {
+              'css',
+              'eruby',
+              'html',
+              'javascript',
+              'javascriptreact',
+              'less',
+              'sass',
+              'scss',
+              'pug',
+              'typescriptreact',
             },
-          },
-        }
-      end,
-    }
-  end,
+            init_options = {
+              includeLanguages = {},
+              excludeLanguages = {},
+              extensionsPath = {},
+              preferences = {},
+              showAbbreviationSuggestions = true,
+              showExpandedAbbreviation = 'always',
+              showSuggestionsAsSnippets = false,
+              syntaxProfiles = {},
+              variables = {},
+            },
+          }
+        end,
+        ['denols'] = function()
+          lspconfig.denols.setup {
+            capabilities = capabilities,
+            root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'),
+          }
+        end,
+        ['ts_ls'] = function()
+          lspconfig.ts_ls.setup {
+            capabilities = capabilities,
+            root_dir = function(fname)
+              local util = lspconfig.util
+              return not util.root_pattern('deno.json', 'deno.jsonc')(fname)
+                and util.root_pattern('tsconfig.json', 'package.json', 'jsconfig.json', '.git')(fname)
+            end,
+            single_file_support = false,
+            init_options = {
+              preferences = {
+                includeCompletionsWithSnippetText = true,
+                includeCompletionsForImportStatements = true,
+              },
+            },
+          }
+        end,
+      }
+    end,
+  },
+
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    dependencies = { 'williamboman/mason.nvim' },
+    config = function()
+      require('mason-tool-installer').setup {
+        ensure_installed = {
+          'prettier',
+          'stylua',
+          'isort',
+          'pylint',
+          'clangd',
+          'denols',
+          { 'eslint_d', version = '13.1.2' },
+        },
+      }
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+  },
+  {
+    'hrsh7th/cmp-nvim-lsp',
+    dependencies = { 'neovim/nvim-lspconfig' },
+  },
 }
